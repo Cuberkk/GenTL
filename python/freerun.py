d8697802d0da22c0131cf5c864c2b914b0ba523e
Kaiwen Zuo <kxz365@case.edu>
2023-11-28 13:12:08 -0500

    point selection

--- python/freerun.py
+++ python/freerun.py
@@ -82,6 +82,7 @@ def freerun():
                     payload = buffer.payload
                     
                     point_cloud_component = payload.components[2]
+                    pointcloud = np.zeros((point_cloud_component.height * point_cloud_component.width, 3))
                     if point_cloud_component.width > 0 and point_cloud_component.height > 0:
                         pointcloud = point_cloud_component.data.reshape(point_cloud_component.height * point_cloud_component.width, 3).copy()
                         # print(pointcloud)
@@ -95,13 +96,13 @@ def freerun():
                     texture_rgb = np.zeros((point_cloud_component.height * point_cloud_component.width, 3))
                     if texture_grey_component.width > 0 and texture_grey_component.height > 0:
                         texture = texture_grey_component.data.reshape(texture_grey_component.height, texture_grey_component.width, 1).copy()
-                        texture_rgb[:, 0] = np.reshape(1/65536 * texture, -1)
-                        texture_rgb[:, 1] = np.reshape(1/65536 * texture, -1)
-                        texture_rgb[:, 2] = np.reshape(1/65536 * texture, -1)
+                        # texture_rgb[:, 0] = np.reshape(1/65536 * texture, -1)
+                        # texture_rgb[:, 1] = np.reshape(1/65536 * texture, -1)
+                        # texture_rgb[:, 2] = np.reshape(1/65536 * texture, -1)
                         
-                        # texture_rgb[:, 0] = np.reshape(texture, -1)
-                        # texture_rgb[:, 1] = np.reshape(texture, -1)
-                        # texture_rgb[:, 2] = np.reshape(texture, -1)
+                        texture_rgb[:, 0] = np.reshape(texture, -1)
+                        texture_rgb[:, 1] = np.reshape(texture, -1)
+                        texture_rgb[:, 2] = np.reshape(texture, -1)
                     elif texture_rgb_component.width > 0 and texture_rgb_component.height > 0:
                         texture = texture_rgb_component.data.reshape(texture_rgb_component.height, texture_rgb_component.width, 3).copy()
                         texture_rgb[:, 0] = np.reshape(1/65536 * texture[:, :, 0], -1)
@@ -109,17 +110,41 @@ def freerun():
                         texture_rgb[:, 2] = np.reshape(1/65536 * texture[:, :, 2], -1)
                     else:
                         print("TextureRGB is empty!")
-                    max_value = np.max(texture_rgb)
-                    print(max_value)
+                        
+                    #First filtration using Point Cloud Data
+                    pointcloud_1st_edition = pointcloud
+                    pointcloud_1st_edition[np.logical_or(pointcloud_1st_edition[: , 0] < 48.5 , pointcloud_1st_edition[: , 0] > 59.5)] = 0 #Filt with X position data
+                    pointcloud_1st_edition[np.logical_or(pointcloud_1st_edition[: , 1] < 131.5 , pointcloud_1st_edition[: , 1] > 136.5)] = 0 #Filt with Y position data
+                    pointcloud_1st_edition[np.logical_or(pointcloud_1st_edition[: , 2] < 88.5 , pointcloud_1st_edition[: , 2] > 104.5)] = 0 #Filt with Z position data
                     
-                    index_of_max = np.argmax(texture_rgb)
-                    row = index_of_max // texture_rgb.shape[1]
-                    col = index_of_max % texture_rgb.shape[1]
+                    # filtered_points = pointcloud[np.any(pointcloud != 0, axis=1)]
+                    first_filtered_points = np.sum(np.any(pointcloud_1st_edition != 0, axis=1))
+                    print("There are:", first_filtered_points, "points after 1st filtration")
                     
-                    print("Position at row:", row, "col:", col)
-                    print(texture.dtype)
+                    #2nd filtration using Greyscale Channel Information
+                    texture_greyscale = texture_rgb
+                    texture_greyscale[np.all(pointcloud_1st_edition == 0, axis = 1)] = 0
+                    texture_greyscale[(texture_greyscale[: , 0] > 400)] = 0 #Filt with greyscale channel information
+                    second_filtered_points = np.sum(np.any(texture_greyscale != 0, axis=1))
+                    print("There are:", second_filtered_points , "points after 2nd filtration")
                     
-                    time.sleep(10)
+                    #Display the selected points
+                    pointcloud_2nd_edition = pointcloud_1st_edition
+                    pointcloud_2nd_edition[np.all(texture_grey_component == 0, axis=1)] = 0
+                    
+                    
+                    # max_value = np.max(texture_rgb)
+                    # print(max_value)
+                    
+                    # index_of_max = np.argmax(texture_rgb)
+                    # row = index_of_max // texture_rgb.shape[1]
+                    # col = index_of_max % texture_rgb.shape[1]
+                    
+                    # print("Position at row:", row, "col:", col)
+                    # print(texture.dtype)
+                    
+                    
+                    time.sleep(100)
 
 
 if __name__ == "__main__":
